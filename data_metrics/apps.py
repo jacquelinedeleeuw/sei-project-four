384f3280778f16cf05343c1966638d5c27584205
atilla-arslan <atillaa@rocketmail.comm>
2021-04-15 10:38:08 +0100

    set up

--- data_metrics/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DataMetricsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'data_metrics'
--- data_metrics/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class DataMetrics(models.Model):
-    average_rental = models.models.FloatField()
-    
-    
-    def __str__(self):
-        return f"{self.address}"
-        
