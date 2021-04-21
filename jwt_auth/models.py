16e061e82661226cda3ecee6016af1897b5bf413
atilla-arslan <atillaa@rocketmail.comm>
2021-04-21 12:22:31 +0100

    user model changes

--- jwt_auth/models.py
+++ jwt_auth/models.py
@@ -5,4 +5,6 @@ class User(AbstractUser):
     email = models.CharField(max_length=50, unique=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
+    password = models.CharField(max_length=128, verbose_name='password', blank=True)
+    password_confirmation = models.CharField(max_length=128, verbose_name='password', blank=True)
     profile_image = models.CharField(max_length=300, blank=True, default='http://res.cloudinary.com/dyng677ts/image/upload/v1618691793/czguijbf0xi9iterh54p.png')
