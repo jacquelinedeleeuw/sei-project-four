8b2366d9f1139891731021fe2342fbd29051cf6d
Jacqueline de Leeuw <jacqueline.de.leeuw@gmail.com>
2021-04-30 19:56:51 +0200

    deploy

--- project/settings.py
+++ project/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = config['DJANGO_SECRET_KEY']
+SECRET_KEY = "%z4sxfy75%i9u*!1y(g$_rxg&-a_mox-t3@qog2smc0%xf9"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
